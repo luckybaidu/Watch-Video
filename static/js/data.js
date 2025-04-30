cb15ec87853a192c932a05b4110b1f2322fea238
luckybaidu <14921063+luckybaidu@users.noreply.github.com>
2025-04-30 10:27:20 +0800

    Update data.js

--- static/js/data.js
+++ static/js/data.js
@@ -1,5 +1,7 @@
 const routes = [
+  {name: '剖云解析', url: 'https://www.pouyun.com/?url='}, 
   {name: 'BL解析', url: 'https://svip.bljiex.cc/?v='}, 
+  {name: '纯净解析', url: 'https://im1907.top/?jx='},
   {name: '虾米解析', url: 'https://jx.xmflv.com/?url='},
   {name: 'JSON解析', url: 'https://jx.jsonplayer.com/player/?url='},
   {name: '影猫の仓库 ', url: 'https://ymck.pro/search.html?wd='}, 
